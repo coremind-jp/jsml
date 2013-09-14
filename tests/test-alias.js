082b844e08ba06d8cec38239a6849e9765c58ef9
h.nakahara <nakahara@coremind.jp>
2013-09-15 04:41:34 +0900

    修正
    TAB -> SPACE

--- tests/test-alias.js
+++ tests/test-alias.js
@@ -4,9 +4,9 @@ jsml.setAlias("./tests/pathSample", "testalias");
 //設定したパスに置かれているモジュールの読み込み
 jsml.require(["testalias.alias_mod"], function()
 {
-	describe('[TEST] jsml.setAlias, jsml.require, jsml.exports', function()
-	{
-	    it('パッケージルートとなるパスにエイリアスを設定し外部モジュールを読み込むことができる', function() {
+    describe('[TEST] jsml.setAlias, jsml.require, jsml.exports', function()
+    {
+        it('パッケージルートとなるパスにエイリアスを設定し外部モジュールを読み込むことができる', function() {
             expect(jsml.modules["testalias.alias_mod"]).toBe("alias module data here");
         });
     });
--- tests/test-dependence.js
+++ tests/test-dependence.js
@@ -4,20 +4,20 @@ jsml.setAlias("./tests/pathSample", "testalias");
 //設定したパスに置かれているモジュールの読み込み
 jsml.require(["testalias.dependence_mod_1"], function()
 {
-	describe('[TEST] dependence', function()
-	{
-	    it('外部モジュールが依存しているモジュールを暗黙的に読み込むことができる', function() {
+    describe('[TEST] dependence', function()
+    {
+        it('外部モジュールが依存しているモジュールを暗黙的に読み込むことができる', function() {
             expect(jsml.modules["testalias.dependence_mod_1"]).toBe("dependence1 module data here");
 
-			//※testalias.dependence_mod_1はtestalias.dependence_mod_2に依存している
-			//なのでコールバックが呼ばれた際にはdependence_mod_2も読み込まれている
+            //※testalias.dependence_mod_1はtestalias.dependence_mod_2に依存している
+            //なのでコールバックが呼ばれた際にはdependence_mod_2も読み込まれている
             expect(jsml.modules["testalias.dependence_mod_2"]).toBe("dependence2 module data here");
         });
     });
-	describe('[TEST] jsml.concatScript', function()
-	{
-	    it('jsmlで読み込まれた全ての依存モジュールを結合することができる', function() {
-	    	jsml.concatScript();
+    describe('[TEST] jsml.concatScript', function()
+    {
+        it('jsmlで読み込まれた全ての依存モジュールを結合することができる', function() {
+            jsml.concatScript();
         });
     });
 });
